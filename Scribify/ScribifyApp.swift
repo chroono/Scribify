acc6cebd3a745741239d8f35d332be9619e1969a
Emil Piórkowski <e.piorkowski11@gmail.com>
2024-07-23 17:43:47 +0200

    Initial Commit

---
+++ Scribify/ScribifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribifyApp.swift
+//  Scribify
+//
+//  Created by Emil Piórkowski on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScribifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
